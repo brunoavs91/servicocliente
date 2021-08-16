903ec2d0bc007e01f782b6605b1a7ca026b2146b
brunoav91@gmail.com <brunoav91@gmail.com>
2021-08-16 20:11:54 -0300

    iniciando projeto

---
+++ src/main/java/com/boaentrega/servicocliente/ServicoclienteApplication.java
@@ -0,0 +1,13 @@
+package com.boaentrega.servicocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boaentrega/servicocliente/ServicoclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaentrega.servicocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
