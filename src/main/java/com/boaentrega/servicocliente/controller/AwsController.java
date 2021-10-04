5bf7ad4376e27687519315857a2339f60a2323e6
brunoav91@gmail.com <brunoav91@gmail.com>
2021-10-03 23:37:15 -0300

    atualizando

--- src/main/java/com/boaentrega/servicocliente/controller/AwsController.java
+++ src/main/java/com/boaentrega/servicocliente/controller/AwsController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
